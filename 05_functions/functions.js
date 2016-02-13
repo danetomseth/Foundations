21808ef64a0d368e4b499e0319c8d49c82bd2833
Dane Tomseth <danetomseth@gmail.com>
2016-02-12 20:48:19 -0800

    function add

--- 05_functions/functions.js
+++ 05_functions/functions.js
@@ -1,1 +1,70 @@
 // Functions.js
+var concat_string = function() {
+	var argArray = Array.prototype.slice.call(arguments);
+	argArray = argArray.join("");
+	return argArray;
+	
+};
+
+var yourFunctionRunner = function(func1, func2) {
+	var string = "";
+	string += func1();
+	if(typeof func2 != 'undefined') {
+		string += func2();
+	}
+	
+	return string;
+	//passed strings from callThisFunction and andThisFunction
+
+};
+
+
+var makeAdder = function(x) {
+    	var storeValue = x;
+    var addFunc = function(x) {
+        storeValue += x;
+        console.log(storeValue);
+        return storeValue;
+    };
+    return addFunc;
+};
+
+var once = function(func) {
+	var count = 0;
+   var changeFunc = function() {
+       var stopOnce = function() {
+       
+    };
+       if(count ===0) {
+           func();
+           count++;
+       }
+       else {
+           increment = stopOnce;
+       }
+   };
+   return changeFunc;
+   
+};
+
+
+var createObjectWithTwoClosures = function() {
+	var createObj = function() {
+	
+	var obj = {};
+		
+	obj.oneIncrementor = function(x) {
+		
+	};
+	obj.tensIncrementor = function(x) {
+		
+	};
+	obj.getValue = function() {
+		
+	};
+	return obj;
+	};	
+	var objStore = createObj;
+    
+};
+
